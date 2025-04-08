707440a155a93e63461527035fc82596591fb381
EvanSmith93 <evan.smith993@gmail.com>
2025-04-08 17:33:46 -0600

    remove the chaos

--- sqlInjection.sh
+++ sqlInjection.sh
@@ -1,8 +1,8 @@
 host=http://localhost:3000
 # host=https://pizza-service.rhythum.click
 
-response=$(curl -s -X POST $host/api/auth -d "{\"name\":\"Your Mom\", \"email\":\"b@jwt.com\", \"password\":\"diner\"}" -H 'Content-Type: application/json')
-# response=$(curl -s -X PUT $host/api/auth -d "{\"email\":\"b@jwt.com\", \"password\":\"diner\"}" -H 'Content-Type: application/json')
+# response=$(curl -s -X POST $host/api/auth -d "{\"name\":\"Your Mom\", \"email\":\"b@jwt.com\", \"password\":\"diner\"}" -H 'Content-Type: application/json')
+response=$(curl -s -X PUT $host/api/auth -d "{\"email\":\"b@jwt.com\", \"password\":\"diner\"}" -H 'Content-Type: application/json')
 echo $response
 id=$(echo $response | jq -r '.user.id')
 token=$(echo $response | jq -r '.token')
